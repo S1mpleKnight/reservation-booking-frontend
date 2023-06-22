f86b82df236abd73c09509bd0c1867edc7da6421
S1mpleKnight <s1mpleknight@mail.ru>
2023-06-22 13:51:37 +0300

    new : create units parts

--- src/components/screens/offer/ExactOffer.jsx
+++ src/components/screens/offer/ExactOffer.jsx
@@ -137,6 +137,99 @@ function ExactOffer() {
             })
     }
 
+    const changeUnitedPart = (e) => {
+        const partName = e.target.value
+        if (unitedParts.length > 0) {
+            const resultPart = unitedParts.find(up => up.name === partName)
+            setUnit(prev => ({
+                ...prev, reservationUnitedPart : resultPart, reservationUnitedPartId : resultPart.id
+            }))
+        }
+    }
+    
+    const changeUnitType = (e) => {
+        const typeName = e.target.value
+        if (unitTypes.length > 0) {
+            const resultType = unitTypes.find(ut => ut.name === typeName)
+            setUnit(prev => ({
+                ...prev, reservationUnitType : resultType, reservationUnitTypeId : resultType.id
+            }))
+        }
+    }
+
+    const changeHasUnitedPart = (e) => {
+        if (unitedParts.length) {
+            setUnit(prev => ({
+                ...prev, hasUnitedPart : e.target.checked     
+            }))
+            if (e.target.checked && !unit.unitedPart) {
+                setUnit(prev => ({
+                    ...prev, reservationUnitedPart : unitedParts[0], reservationUnitedPartId : unitedParts[0].id
+                }))
+            }
+        } else {
+            setUnit(prev => ({
+                ...prev, hasUnitedPart : false, reservationUnitedPart : {
+                    name : '',
+                    id : ''
+                }     
+            }))
+        }
+    }
+
+    const changeHasReservationType = (e) => {
+        if (unitTypes.length) {
+            setUnit(prev => ({
+                ...prev, hasReservationUnitType : e.target.checked     
+            }))
+            if (e.target.checked && !unit.unitType) {
+                setUnit(prev => ({
+                    ...prev, reservationUnitType : unitTypes[0], reservationUnitTypeId : unitTypes[0].id
+                }))
+            }
+        } else {
+            setUnit(prev => ({
+                ...prev, hasReservationUnitType : false, reservationUnitType : {
+                    name : '',
+                    id : ''
+                }     
+            }))
+        }
+    }
+
+    const createUnit = async (e) => {
+        e.preventDefault()
+        setError('')
+        if (!unit.hasTime) {
+            setUnit(prev => ({
+                ...prev, time : ''
+            }))
+        }
+        await UnitsService.create(unit, user.token, id)
+            .then(function(response) {
+                setUnits([
+                    ...units, response.data
+                ])
+                setUnit('')
+            })
+            .catch(function(errorMessage) {
+                setError(errorMessage)
+            })
+    }
+
+    const deleteUnit = async (e) => {
+        setError('')
+        await UnitsService.delete(id, user.token, e.id)
+            .then(function(response) {
+                setUnits(
+                    units.filter(u => u.id !== e.id)
+                )
+            })
+            .catch(function(errorMessage) {
+                setError(errorMessage)
+            })
+    }
+
     return (
         <>
             <NavigationBar/>
@@ -377,6 +470,229 @@ function ExactOffer() {
                             <div className="d-flex justify-content-center">
                                 <Alert variant='light' className="mt-2">
                                     There are no united parts
+                                </Alert>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className="border border-dark px-3 py-2 mt-3">   
+                    <h5>Reservation units</h5>
+                    <Form>
+                        <FormGroup>
+                            <Row>
+                                <Col xs={2}>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Label>Name</Form.Label>
+                                    </div>
+                                </Col>
+                                <Col xs={10}>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter unit name"
+                                        value={unit.name}
+                                        onChange={e => setUnit(prev => ({
+                                            ...prev, name : e.target.value, offerId : id
+                                        }))}/>
+                                </Col>
+                            </Row>
+                        </FormGroup>
+                        <FormGroup>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <Form.Label>Description</Form.Label>
+                            </div>
+                            <Form.Control
+                                as="textarea"
+                                placeholder="Leave a comment here"
+                                style={{ height: '100px' }}
+                                value={unit.description}
+                                onChange={e => setUnit(prev => ({
+                                    ...prev, description : e.target.value
+                                }))}
+                            />
+                        </FormGroup>
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Label>Has united part?</Form.Label>
+                                    </div>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Check 
+                                            type="switch" 
+                                            value={unit.hasUnitedPart} 
+                                            onChange={e => changeHasUnitedPart(e)}/>
+                                    </div>
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                { unit.hasUnitedPart && 
+                                    <FormGroup>
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Label>United part</Form.Label>
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Select
+                                                onChange={e => changeUnitedPart(e)}
+                                                value={unit.reservationUnitedPart.name}>
+                                                {unitedParts.map(up => (
+                                                    <option key={up.id}>{up.name}</option>
+                                                ))}
+                                           </Form.Select>
+                                        </div>
+                                    </FormGroup>
+                                }
+                            </Col>
+                            <Col>
+                                <FormGroup>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Label>Has time?</Form.Label>
+                                    </div>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Check 
+                                            type="switch" 
+                                            value={unit.hasTime} 
+                                            onChange={e => setUnit(prev => ({
+                                                ...prev, hasTime : e.target.checked
+                                        }))}/>
+                                    </div>
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                { unit.hasTime && 
+                                    <FormGroup className="time">
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Label>Time</Form.Label>
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Control 
+                                                type="text" 
+                                                placeholder="Enter time in format hh:mm" 
+                                                onChange={e => setUnit(prev => ({
+                                                    ...prev, time : e.target.value
+                                                }))}
+                                                value={unit.time}/>
+                                        </div>
+                                    </FormGroup>
+                                }
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Label>Has order number?</Form.Label>
+                                    </div>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Check 
+                                            type="switch" 
+                                            value={unit.hasOrderNumber} 
+                                            onChange={e => setUnit(prev => ({
+                                                ...prev, hasOrderNumber : e.target.checked
+                                        }))}/>
+                                    </div>
+                                </FormGroup>
+                            
+                            </Col>
+                            {unit.hasOrderNumber &&
+                                <Col>
+                                    <FormGroup className="event">
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Label>Order number</Form.Label>
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Control 
+                                                type="text" 
+                                                placeholder="Enter order number" 
+                                                onChange={e => setUnit(prev => ({
+                                                    ...prev, orderNumber : e.target.value
+                                                }))}
+                                                value={unit.orderNumber}/>
+                                        </div>
+                                    </FormGroup>
+                                </Col>
+                            }
+                            <Col>
+                                <FormGroup>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Label>Has unit type?</Form.Label>
+                                    </div>
+                                    <div className="d-flex align-items-center justify-content-center">
+                                        <Form.Check 
+                                            type="switch" 
+                                            value={unit.hasReservationUnitType} 
+                                            onChange={e => changeHasReservationType(e)}/>
+                                    </div>
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                { unit.hasReservationUnitType && 
+                                    <FormGroup>
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Label>Unit type</Form.Label>
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-center">
+                                            <Form.Select
+                                                onChange={e => changeUnitType(e)}
+                                                value={unit.reservationUnitType.name}>
+                                                {unitTypes.map(ut => (
+                                                    <option key={ut.id}>{ut.name}</option>
+                                                ))}
+                                           </Form.Select>
+                                        </div>
+                                    </FormGroup>
+                                }
+                            </Col>
+                        </Row>
+                        <div className="d-flex align-items-center justify-content-center mt-2">
+                            <Button onClick={e => createUnit(e)}>
+                                Create
+                            </Button>
+                        </div>
+                    </Form>
+                    <div className="mt-2">
+                        {units.length ? (
+                            <Table striped hover>
+                                <tbody>
+                                    <tr>
+                                        <th>Unit name</th>
+                                        <th>United part</th>
+                                        <th>Time</th>
+                                        <th>Order number</th>
+                                        <th>Type</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                    {units.map(u => (
+                                        <tr key={u.id}>
+                                            <td>{u.name}</td>
+                                            <td>{u.hasUnitedPart ? u.reservationUnitedPart.name : "N/A"}</td>
+                                            <td>{u.hasTime ? u.time : "N/A"}</td>
+                                            <td>{u.hasOrderNumber ? u.orderNumber : "N/A"}</td>
+                                            <td>{u.hasReservationUnitType ? u.reservationUnitType.name : "N/A"}</td>
+                                            <td className="d-flex justify-content-start">
+                                                <div>
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                            width="16"
+                                                            height="16"
+                                                            fill="currentColor"
+                                                            className="bi bi-trash mx-2"
+                                                            viewBox="0 0 16 16"
+                                                            onClick={() => deleteUnit(u)}
+                                                    >
+                                                        <path
+                                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                                        <path fillRule="evenodd"
+                                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                                    </svg>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))} 
+                                </tbody>
+                            </Table>
+                        ) : (
+                            <div className="d-flex justify-content-center">
+                                <Alert variant='light'>
+                                    There are no units
                                 </Alert>
                             </div>
                         )}
